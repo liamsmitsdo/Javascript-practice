41083c79e43b716981c18e7212a94b4a57388a12
liamsmitsdo <liamsmitsdorff@gmail.com>
2022-01-28 13:57:16 +0200

    added ajax short hand for jQuery

--- AJAX_2/jQuery/app.js
+++ AJAX_2/jQuery/app.js
@@ -11,4 +11,24 @@ $('#btn').click(function(){
     })
 })
 
+//shorthand ajax functions
+$('#get').click(function(){
+    $.get('https://api.github.com/users/liamsmitsdo')
+    .done(function(data){
+        console.log(data)
+    })
+    .fail(e=>{console.log(e)})
+})
 
+$('#post').click(function(){
+    let data = {name: 'text', city: 'text2'}
+    $.post('www.fakeurl.com', data)
+    .done(data => console.log('hi'))
+    .fail(e=>console.log(e.statusText))
+})
+
+$('#getJson').click(function(){
+    $.getJSON('https://api.github.com/users/liamsmitsdo')
+    .done(data => console.log(data))
+    .fail(e=>console.log(e.statusText))
+})
