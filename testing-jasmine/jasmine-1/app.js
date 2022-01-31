2a8cf4c16c2f23da726ca1872e86b1c64a34ec38
liamsmitsdo <liamsmitsdorff@gmail.com>
2022-01-31 07:45:38 +0200

    added the hooks practice for jasmine

--- testing-jasmine/jasmine-1/app.js
+++ testing-jasmine/jasmine-1/app.js
@@ -13,6 +13,25 @@ describe('earth', function(){
     })
 })
 
+describe('Arrays', function(){
+    let arr;
+    //the beforeEach runs code before every it declaration
+    //afterEach would run after every declaration
+    beforeEach(function(){
+        arr = [1,3,5]
+    })
+    it('adds elements to an array', function(){
+        arr.push(7)
+        expect(arr).toEqual([1,3,5,7])
+    })
+    it('returns the new length of the array', function(){
+        expect(arr.push(7)).toBe(4)
+    })
+    it('adds anything into the array', function(){
+        expect(arr.push({})).toBe(4)
+    })
+})
+
 //matchers
 //toBe  not.toBe
 //toBeCloseTo
@@ -21,3 +40,29 @@ describe('earth', function(){
 //toBeGreaterThan toBeLessThan
 //toContain
 //toEqual
+
+//we can nest describe blocks
+describe('arrays', function(){
+    let arr;
+    beforeEach(function(){
+        arr = [1,3,5]
+    })
+
+    describe('#unshift', function(){
+        //it
+        //expects
+    })
+
+    describe('#push', function(){
+        //it
+        //expects
+    })
+})
+
+//pending tests - mark it with an x or add pending() inside it
+
+describe('pending specs', function(){
+    xit('can start with an xit', function(){
+        expect(true).toBe(true)
+    })
+})
