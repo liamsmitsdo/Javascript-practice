1151f0e36a660f3357bf76086684d61af8f4fa89
liamsmitsdo <liamsmitsdorff@gmail.com>
2022-02-01 08:54:36 +0200

    added map array method

--- advanved-array-methods/main.js
+++ advanved-array-methods/main.js
@@ -7,3 +7,8 @@ array.forEach(function(value, index, array) {
 })
 
 //map
+//always need to return within the callback
+const array2 = [2,4,6]
+array2.map(function(value, index, array) {
+    return value / 1
+})
