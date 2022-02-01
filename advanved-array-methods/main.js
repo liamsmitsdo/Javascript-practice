6a0f52d54cdeaf256df83c51a355f366a5f4820d
liamsmitsdo <liamsmitsdorff@gmail.com>
2022-02-01 09:07:44 +0200

    added filter array method

--- advanved-array-methods/main.js
+++ advanved-array-methods/main.js
@@ -1,7 +1,6 @@
 //forEach
 //always return undefined
 const array = [1,2,3]
-
 array.forEach(function(value, index, array) {
     console.log(value)
 })
@@ -12,3 +11,12 @@ const array2 = [2,4,6]
 array2.map(function(value, index, array) {
     return value / 1
 })
+
+//filter
+//result of callback will always be a boolean
+//try not to use if statements
+const array3 = [1,2,3,4,5]
+array3.filter(function(value,index,array) {
+    return value%2===0 //only the values that are true will be in the new array
+})
+
