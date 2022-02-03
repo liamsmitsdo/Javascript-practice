6cdc929c729fd18037e4c89c14eef2c6b298ae63
liamsmitsdo <liamsmitsdorff@gmail.com>
2022-02-03 07:49:11 +0200

    updated OOP section

--- OOP/main.js
+++ OOP/main.js
@@ -8,3 +8,33 @@ function House(bedrooms,bathrooms,numSqft) {
 }
 
 const firstHouse = new House(2,2,1000)
+
+function Dog(name, age) {
+    this.name = name
+    this.age = age
+    this.bark = function bark(){
+        console.log(`${name} just barked!`)
+    }
+}
+
+const milano = new Dog('milano', 5)
+
+//multiple constructor functions
+//this can help reduce duplication
+function Car(make, model, year) {
+    this.make = make
+    this.model = model
+    this.year = year
+    this.numWheels = 4
+}
+
+function Motorcycle(make,model,year) {
+    Car.call(this, make, model, year)  //we use call here so that we dont have to duplicate code
+    this.numWheels = 2
+}
+
+function Motorcycle() {
+    Car.call(this, arguments)  //we can use the arguments keyword instead
+    this.numWheels = 2
+}
+
