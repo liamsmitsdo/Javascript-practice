9451567b605d1e27b1db6578d69ad17cbb96aba0
liamsmitsdo <liamsmitsdorff@gmail.com>
2022-02-10 19:13:51 +0200

    updated OOP.js

--- ES2015/OOP.js
+++ ES2015/OOP.js
@@ -1,17 +1,28 @@
 //class keyword
-class Student {
+class Parent {
     constructor(username,age) {
         this.username = username
         this.age = age
     }
     //instance methods
     greet() {
-        return `Hello ${this.name}, welcome to the class bud`
+        return `Hello ${this.username}, welcome to the class bud!`
     }
     //class methods
-    static isStudent(obj){
-        return obj.constructor === Student
+    static isParent(obj){
+        return obj.constructor === Parent
     }
 }
 
-const liam = new Student('Liam', 24)
+const liam = new Parent('Liam', 24)
+// console.log(liam.greet())
+
+//inheritance
+class Child extends Parent {
+    constructor(username, age) {
+        super(username, age)
+    }
+}
+
+const joshie = new Child('Joshua', 10)
+// console.log(joshie.greet())
